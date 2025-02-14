d39d86c9d386a0c3862b9456b018d7d5e2cbc6d1
chr8stoph <57600205+cbathon@users.noreply.github.com>
2025-02-14 14:38:55 -0800

    added dotenv package and installed dependencies. added openAi key, updated gitignore  and tested server

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": api_key}
+
 
 if __name__ == "__main__":
     import uvicorn
